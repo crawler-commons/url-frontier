7491fb4c439ae1e8829fa2470da21389d262bcb0
Julien Nioche <julien@digitalpebble.com>
2021-06-23 16:18:04 +0100

    Add GetActive endpoint to API, fixes #24

---
+++ client/src/main/java/crawlercommons/urlfrontier/client/GetActive.java
@@ -0,0 +1,51 @@
+/**
+ * SPDX-FileCopyrightText: 2020 Crawler-commons
+ * SPDX-License-Identifier: Apache-2.0
+ * Licensed to Crawler-Commons under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * DigitalPebble licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package crawlercommons.urlfrontier.client;
+
+import crawlercommons.urlfrontier.URLFrontierGrpc;
+import crawlercommons.urlfrontier.URLFrontierGrpc.URLFrontierBlockingStub;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.ParentCommand;
+
+@Command(name = "GetActive", description = "Check whether the Frontier has been paused")
+public class GetActive implements Runnable {
+
+	@ParentCommand
+	private Client parent;
+
+	@Override
+	public void run() {
+		ManagedChannel channel = ManagedChannelBuilder.forAddress(parent.hostname, parent.port).usePlaintext().build();
+
+		URLFrontierBlockingStub blockingFrontier = URLFrontierGrpc.newBlockingStub(channel);
+
+		crawlercommons.urlfrontier.Urlfrontier.Empty.Builder builder = crawlercommons.urlfrontier.Urlfrontier.Empty
+				.newBuilder();
+
+		crawlercommons.urlfrontier.Urlfrontier.Boolean b = blockingFrontier.getActive(builder.build());
+
+		System.out.println("Active: " + b.getState());
+
+		channel.shutdownNow();
+	}
+
+}
