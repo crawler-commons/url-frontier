95effb0b9df0ca0ed0bf6cb2da74a7c9b5215e4a
Julien Nioche <julien@digitalpebble.com>
2021-07-21 15:16:50 +0100

    pre 0.3 release

--- service/src/main/java/crawlercommons/urlfrontier/service/URLFrontierServer.java
+++ service/src/main/java/crawlercommons/urlfrontier/service/URLFrontierServer.java
@@ -41,7 +41,7 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-@Command(name = "URL Frontier Server", mixinStandardHelpOptions = true, version = "0.2")
+@Command(name = "URL Frontier Server", mixinStandardHelpOptions = true, version = "0.3")
 public class URLFrontierServer implements Callable<Integer> {
 
 	private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(URLFrontierServer.class);
