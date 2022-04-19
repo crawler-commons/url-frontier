a53f50ec5853fdafc858728fc2ef6ac1a25e862d
Julien Nioche <julien@digitalpebble.com>
2022-04-19 17:07:29 +0100

    Adding missing methods to DistributedFrontierService
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>

--- service/src/main/java/crawlercommons/urlfrontier/service/cluster/DistributedFrontierService.java
+++ service/src/main/java/crawlercommons/urlfrontier/service/cluster/DistributedFrontierService.java
@@ -14,26 +14,43 @@
  */
 package crawlercommons.urlfrontier.service.cluster;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
 import crawlercommons.urlfrontier.CrawlID;
 import crawlercommons.urlfrontier.URLFrontierGrpc;
 import crawlercommons.urlfrontier.URLFrontierGrpc.URLFrontierBlockingStub;
 import crawlercommons.urlfrontier.Urlfrontier.DeleteCrawlMessage;
+import crawlercommons.urlfrontier.Urlfrontier.Empty;
+import crawlercommons.urlfrontier.Urlfrontier.Local;
+import crawlercommons.urlfrontier.Urlfrontier.LogLevelParams;
+import crawlercommons.urlfrontier.Urlfrontier.Pagination;
+import crawlercommons.urlfrontier.Urlfrontier.QueueList;
 import crawlercommons.urlfrontier.Urlfrontier.QueueWithinCrawlParams;
 import crawlercommons.urlfrontier.Urlfrontier.Stats;
+import crawlercommons.urlfrontier.Urlfrontier.StringList;
 import crawlercommons.urlfrontier.service.AbstractFrontierService;
+import crawlercommons.urlfrontier.service.QueueInterface;
 import crawlercommons.urlfrontier.service.QueueWithinCrawl;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import org.slf4j.LoggerFactory;
 
 public abstract class DistributedFrontierService extends AbstractFrontierService {
 
     private static final org.slf4j.Logger LOG =
             LoggerFactory.getLogger(DistributedFrontierService.class);
+
+    private URLFrontierBlockingStub getFrontier(String target) {
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+        return URLFrontierGrpc.newBlockingStub(channel);
+    }
 
     /** Delete the queue based on the key in parameter */
     @Override
@@ -52,9 +69,7 @@ public abstract class DistributedFrontierService extends AbstractFrontierService
                 // force to local so that remote node don't go recursive
                 QueueWithinCrawlParams local =
                         QueueWithinCrawlParams.newBuilder(request).setLocal(true).build();
-                ManagedChannel channel =
-                        ManagedChannelBuilder.forTarget(node).usePlaintext().build();
-                URLFrontierBlockingStub blockingFrontier = URLFrontierGrpc.newBlockingStub(channel);
+                URLFrontierBlockingStub blockingFrontier = getFrontier(node);
                 crawlercommons.urlfrontier.Urlfrontier.Long total =
                         blockingFrontier.deleteQueue(local);
                 sizeQueue += total.getValue();
@@ -91,9 +106,7 @@ public abstract class DistributedFrontierService extends AbstractFrontierService
             for (String node : nodes) {
                 if (node.equals(address)) continue;
                 // call the delete endpoint in the target node
-                ManagedChannel channel =
-                        ManagedChannelBuilder.forTarget(node).usePlaintext().build();
-                URLFrontierBlockingStub blockingFrontier = URLFrontierGrpc.newBlockingStub(channel);
+                URLFrontierBlockingStub blockingFrontier = getFrontier(node);
                 crawlercommons.urlfrontier.Urlfrontier.Long localCount =
                         blockingFrontier.deleteCrawl(local);
                 total += localCount.getValue();
@@ -131,9 +144,7 @@ public abstract class DistributedFrontierService extends AbstractFrontierService
         QueueWithinCrawlParams local =
                 QueueWithinCrawlParams.newBuilder(request).setLocal(true).build();
         for (String node : nodes) {
-            // call the delete endpoint in the target node
-            ManagedChannel channel = ManagedChannelBuilder.forTarget(node).usePlaintext().build();
-            URLFrontierBlockingStub blockingFrontier = URLFrontierGrpc.newBlockingStub(channel);
+            URLFrontierBlockingStub blockingFrontier = getFrontier(node);
             Stats localStats = blockingFrontier.getStats(local);
             numQueues += localStats.getNumberOfQueues();
             size += localStats.getSize();
@@ -157,6 +168,89 @@ public abstract class DistributedFrontierService extends AbstractFrontierService
                         .setCrawlID(normalisedCrawlID)
                         .build();
         responseObserver.onNext(stats);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void setLogLevel(LogLevelParams request, StreamObserver<Empty> responseObserver) {
+
+        if (!request.getLocal()) {
+            // force to local so that remote node don't go recursive
+            LogLevelParams local = LogLevelParams.newBuilder(request).setLocal(true).build();
+            for (String node : nodes) {
+                // exclude the local node
+                if (node.equals(address)) continue;
+                URLFrontierBlockingStub blockingFrontier = getFrontier(node);
+                blockingFrontier.setLogLevel(local);
+            }
+        }
+
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        ch.qos.logback.classic.Logger logger = loggerContext.getLogger(request.getPackage());
+        logger.setLevel(Level.toLevel(request.getLevel().toString()));
+        LOG.info("Log level for {} set to {}", request.getPackage(), request.getLevel().toString());
+        responseObserver.onNext(Empty.getDefaultInstance());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void listCrawls(
+            crawlercommons.urlfrontier.Urlfrontier.Local request,
+            io.grpc.stub.StreamObserver<crawlercommons.urlfrontier.Urlfrontier.StringList>
+                    responseObserver) {
+
+        Set<String> crawlIDs = new HashSet<>();
+
+        if (!request.getLocal()) {
+            // force to local so that remote node don't go recursive
+            Local local = Local.newBuilder().setLocal(true).build();
+            for (String node : nodes) {
+                // exclude the local node
+                if (node.equals(address)) continue;
+                URLFrontierBlockingStub blockingFrontier = getFrontier(node);
+                StringList results = blockingFrontier.listCrawls(local);
+                for (String s : results.getValuesList()) {
+                    crawlIDs.add(s);
+                }
+            }
+        }
+
+        synchronized (queues) {
+            Iterator<Entry<QueueWithinCrawl, QueueInterface>> iterator =
+                    queues.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Entry<QueueWithinCrawl, QueueInterface> e = iterator.next();
+                crawlIDs.add(e.getKey().getCrawlid());
+            }
+        }
+        responseObserver.onNext(StringList.newBuilder().addAllValues(crawlIDs).build());
+        responseObserver.onCompleted();
+    }
+
+    public void listQueues(
+            crawlercommons.urlfrontier.Urlfrontier.Pagination request,
+            io.grpc.stub.StreamObserver<crawlercommons.urlfrontier.Urlfrontier.QueueList>
+                    responseObserver) {
+
+        if (request.getLocal()) {
+            super.listQueues(request, responseObserver);
+            return;
+        }
+
+        Set<String> dedup = new HashSet<>();
+
+        Pagination localPagination = Pagination.newBuilder(request).setLocal(true).build();
+        for (String node : nodes) {
+            URLFrontierBlockingStub blockingFrontier = getFrontier(node);
+            QueueList listqueues = blockingFrontier.listQueues(localPagination);
+            for (String s : listqueues.getValuesList()) {
+                dedup.add(s);
+            }
+        }
+
+        crawlercommons.urlfrontier.Urlfrontier.QueueList.Builder list = QueueList.newBuilder();
+        list.addAllValues(dedup);
+        responseObserver.onNext(list.build());
         responseObserver.onCompleted();
     }
 }
