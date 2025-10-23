3d428b3294aaeea33b802feef11c2461187482e8
Laurent Klock <Laurent.Klock@arhs-cube.com>
2025-10-23 14:37:00 +0200

    Updated version to 2.6-SNAPSHOT
    Signed-off-by: Laurent Klock <Laurent.Klock@arhs-cube.com>

--- service/src/main/java/crawlercommons/urlfrontier/service/URLFrontierServer.java
+++ service/src/main/java/crawlercommons/urlfrontier/service/URLFrontierServer.java
@@ -25,7 +25,7 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-@Command(name = "URL Frontier Server", mixinStandardHelpOptions = true, version = "2.5")
+@Command(name = "URL Frontier Server", mixinStandardHelpOptions = true, version = "2.6-SNAPSHOT")
 public class URLFrontierServer implements Callable<Integer> {
 
     private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(URLFrontierServer.class);
